783c3b9677dd1b3303acc107542d765f3835bb28
bMsithole19980 <bmsithole1998@gmail.com>
2024-11-20 10:55:53 +0200

    hambuger-icon modification

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,90 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes, FaChevronDown , FaGraduationCap , FaClipboardList , FaHandHolding } from "react-icons/fa"; // Import React Icons for Hamburger, Close, and Down Arrow
+import {
+  FaBars,
+  FaBuilding,
+  FaBriefcase,
+  FaTimes,
+  FaChevronDown,
+  FaGraduationCap,
+  FaClipboardList,
+  FaUsers,
+  FaHandHolding,
+} from "react-icons/fa";
 import "./Navbar.css";
-import Logo from '../Images/Ikusasa.png';
+import Logo from "../Images/Ikusasa.png";
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
-  const [isServicesOpen, setIsServicesOpen] = useState(false); // For services dropdown menu
-  const [isAboutOpen, setIsAboutOpen] = useState(false); // For about dropdown menu
+  const [isServicesOpen, setIsServicesOpen] = useState(false);
+  const [isAboutOpen, setIsAboutOpen] = useState(false);
 
   return (
     <nav className="navbar">
+      {/* Logo Section */}
       <div className="logo">
         <img src={Logo} alt="ikusasa-logo" />
       </div>
+
+      {/* Navigation Links */}
       <ul className={`nav-links ${isMobile ? "nav-links-mobile" : ""}`}>
         <li>
           <a href="/">Home</a>
         </li>
-        <li 
+        <li
           className="dropdown"
-          onMouseEnter={() => setIsServicesOpen(true)} // Show dropdown on hover
-          onMouseLeave={() => setIsServicesOpen(false)} // Hide dropdown when hover ends
+          onMouseEnter={() => !isMobile && setIsServicesOpen(true)}
+          onMouseLeave={() => !isMobile && setIsServicesOpen(false)}
+          onClick={() => isMobile && setIsServicesOpen(!isServicesOpen)}
         >
           <a href="/services">
             Services <FaChevronDown />
           </a>
           {isServicesOpen && (
             <ul className="dropdown-menu">
-                <ul className="dropdown-menu">
-              <li><a href="/services/training"><FaGraduationCap style={{ marginRight: '10px' }} /> Training</a></li> {/* Icon added here */}
-              <li><a href="/services/reseller"><FaClipboardList style={{ marginRight: '10px' }} /> Reseller</a></li> {/* Icon added here */}
-              <li><a href="/services/consulting"><FaHandHolding style={{ marginRight: '10px' }} /> Consulting</a></li> {/* Icon added here */}
-            </ul>
+              <li>
+                <a href="/services/training">
+                  <FaGraduationCap style={{ marginRight: "10px" }} /> Training
+                </a>
+              </li>
+              <li>
+                <a href="/services/reseller">
+                  <FaClipboardList style={{ marginRight: "10px" }} /> Reseller
+                </a>
+              </li>
+              <li>
+                <a href="/services/consulting">
+                  <FaHandHolding style={{ marginRight: "10px" }} /> Consulting
+                </a>
+              </li>
             </ul>
           )}
         </li>
         <li
           className="dropdown"
-          onMouseEnter={() => setIsAboutOpen(true)} // Show dropdown on hover
-          onMouseLeave={() => setIsAboutOpen(false)} // Hide dropdown when hover ends
+          onMouseEnter={() => !isMobile && setIsAboutOpen(true)}
+          onMouseLeave={() => !isMobile && setIsAboutOpen(false)}
+          onClick={() => isMobile && setIsAboutOpen(!isAboutOpen)}
         >
           <a href="/about">
             About <FaChevronDown />
           </a>
           {isAboutOpen && (
             <ul className="dropdown-menu">
-              <li><a href="/about/company">Company</a></li>
-              <li><a href="/about/team">Team</a></li>
-              <li><a href="/about/career">Career</a></li>
+              <li>
+                <a href="/about/company">
+                  <FaBuilding style={{ marginRight: "10px" }} /> Company
+                </a>
+              </li>
+              <li>
+                <a href="/about/team">
+                  <FaUsers style={{ marginRight: "10px" }} /> Team
+                </a>
+              </li>
+              <li>
+                <a href="/about/career">
+                  <FaBriefcase style={{ marginRight: "10px" }} /> Career
+                </a>
+              </li>
             </ul>
           )}
         </li>
@@ -64,7 +101,12 @@ const Navbar = () => {
           <a href="/gallery">Gallery</a>
         </li>
       </ul>
-      <button className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
+
+      {/* Hamburger Icon for Mobile */}
+      <button
+        className="mobile-menu-icon"
+        onClick={() => setIsMobile(!isMobile)}
+      >
         {isMobile ? <FaTimes /> : <FaBars />}
       </button>
     </nav>
